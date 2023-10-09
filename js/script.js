724c951a733b3b75d0f8e2933e5f10c975b209d8
angelo friello <angelofriello01@gmail.com>
2023-10-09 15:38:23 +0200

    valid -email

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ let email;
 email = prompt ('digita la tua email');
 console.log(email)
 // - dichiarare un array con le email registrate
-const validEmail = ['pasquale@gmail.com','andrea@gmail.com','francesco@gmail.com','giuseppe@gmail.com','marco@gmail.com']
+let validEmail = ['pasquale@gmail.com','andrea@gmail.com', 'pippo@pluto.com', 'michele@gmail.com','dario@gmail.com'];
 // - dichiarare che un email puo accedere solo se registrata all'interno dell'array
-// - mandara un messaggio che dice se è stato ferificata l'email
+//     - mandara un messaggio che dice se è stato ferificata l'email
+let messageDomElement = document.getElementById('email');
+let firstPartMessage = ('La tua email è:');
+let valid = ('valida');
+let notValid = ('non è valida');
+
+let actualEmail;
+
+for (let i = 0; i < validEmail.length; i++) {
+
+    actualEmail = validEmail[i];
+
+    if (actualEmail === email) {
+        console.log('valida');
+        messageDomElement.innerHTML = firstPartMessage + valid
+
+    }
+}
+  // console.log(messageDomElement);
